11f8b85e68de7dfe18663995cec66f0a9b293f44
MbPro13_chudikO <oleg.chudnovskiy.gmail.com>
2022-02-01 14:15:37 +0200

    Initial commit

---
+++ app/src/main/java/com/chudnovskiy/geoquizapp/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.chudnovskiy.geoquizapp
+
+import android.graphics.Rect
+import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: ImageButton
+    private lateinit var prevButton: ImageButton
+    private lateinit var questionTextView: TextView
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+    )
+
+    private var currentIndex = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        initViews()
+
+        trueButton.setOnClickListener() {
+//            displayToastAboveButton(trueButton, R.string.correct_toast)
+            checkAnswer(true)
+        }
+        falseButton.setOnClickListener {
+//            displayToastAboveButton(falseButton, R.string.incorrect_toast)
+            checkAnswer(false)
+        }
+        nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        val questionTextResId = questionBank[currentIndex].textResId
+        questionTextView.setText(questionTextResId)
+    }
+
+    private fun checkAnswer(userAnswer: Boolean) {
+        val correctAnswer = questionBank[currentIndex].answer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show()
+    }
+
+    private fun initViews() {
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+        nextButton = findViewById(R.id.next_button)
+        questionTextView = findViewById(R.id.question_text_view)
+    }
+
+    // v is the Button view that you want the Toast to appear above
+    // and messageId is the id of your string resource for the message
+    // https://stackoverflow.com/questions/2506876/how-to-change-position-of-toast-in-android
+    private fun displayToastAboveButton(v: View, messageId: Int) {
+        var xOffset = 0
+        var yOffset = 0
+        val gvr = Rect()
+        val parent: View = v.getParent() as View
+        val parentHeight: Int = parent.getHeight()
+        if (v.getGlobalVisibleRect(gvr)) {
+            val root: View = v.getRootView()
+            val halfWidth: Int = root.getRight() / 2
+            val halfHeight: Int = root.getBottom() / 2
+            val parentCenterX: Int = (gvr.right - gvr.left) / 2 + gvr.left
+            val parentCenterY: Int = (gvr.bottom - gvr.top) / 2 + gvr.top
+            yOffset = if (parentCenterY <= halfHeight) {
+                -(halfHeight - parentCenterY) - parentHeight
+            } else {
+                parentCenterY - halfHeight - parentHeight
+            }
+            if (parentCenterX < halfWidth) {
+                xOffset = -(halfWidth - parentCenterX)
+            }
+            if (parentCenterX >= halfWidth) {
+                xOffset = parentCenterX - halfWidth
+            }
+        }
+        val toast = Toast.makeText(this, messageId, Toast.LENGTH_SHORT)
+        toast.setGravity(Gravity.CENTER, xOffset, yOffset)
+        toast.show()
+    }
+}
